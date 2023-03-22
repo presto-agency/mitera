880f7dcd237692128d1dc5abf91ba0f272b881d0
Kamarhinanton <Kamarhin_anton@ukr.net>
2023-03-22 11:04:50 +0200

    fix: nav menu mobile

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -223,6 +223,51 @@
         swiperLogosSlider.destroy();
       }
     },
+
+    showSubMenu: function (itemsLinks) {
+
+      function setHeight(itemsLinks) {
+        if (itemsLinks.length > 0) {
+          itemsLinks.forEach(item => {
+            let itemLink = item.querySelector('a');
+            if (!!itemLink) {
+              itemLink.onclick = (e) => {
+                console.log(e)
+                e.preventDefault()
+                let itemsMenu = item.querySelectorAll('ul>li');
+                let submenuHeight = getHeight(itemsMenu)
+                let active = item.parentNode.parentNode.querySelector('.active');
+                let wrapper = itemsMenu[0].parentNode.parentNode;
+                if (!!active && active !== wrapper) {
+                  active.querySelector('ul').style.maxHeight = 0 + 'px';
+                  active.classList.remove('active');
+                  checkHeight(itemsMenu[0], submenuHeight);
+                  wrapper.classList.add('active')
+                } else if (!!active && active === wrapper) {
+                  active.querySelector('ul').style.maxHeight = 0 + 'px';
+                  active.classList.remove('active');
+                } else {
+                  checkHeight(itemsMenu[0], submenuHeight)
+                  wrapper.classList.add('active')
+                }
+              }
+            }
+          })
+        }
+      }
+
+      function getHeight(items) {
+        let subHeight = Array.from(items).reduce((sum, current) =>
+          sum + current.offsetHeight, 0);
+        return subHeight
+      }
+
+      function checkHeight(item, height) {
+        item.parentNode.style.maxHeight = height + 'px';
+      }
+
+      setHeight(itemsLinks)
+    }
   }
   //init
   app.initImageSlider();
@@ -258,6 +303,9 @@
     //listeners
     app.mediaQueryDesktop.addEventListener('change', app.handleTabletChange);
     app.handleTabletChange(app.mediaQueryDesktop);
+    if (window.innerWidth < 1200){
+      app.showSubMenu(app.header.querySelectorAll('.menu-item-has-children'))
+    }
   });
 
   const anchorLinks = document.querySelectorAll('.scroll-to');
